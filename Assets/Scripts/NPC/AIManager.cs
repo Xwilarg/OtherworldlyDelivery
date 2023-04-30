b59c3bca74ff49c10dda5c03a79c0635fcee81cf
Christian Chaux <xwilarg@protonmail.com>
2023-04-30 20:14:13 +0200

    Improve AI

--- Assets/Scripts/NPC/AIManager.cs
+++ Assets/Scripts/NPC/AIManager.cs
@@ -22,6 +22,8 @@ namespace LudumDare53.NPC
 
         private bool _canUseUlt = true;
 
+        private int _waitBeforeAttack = 2;
+
         private void Awake()
         {
             Instance = this;
@@ -39,8 +41,20 @@ namespace LudumDare53.NPC
             else
             {
                 deck = CardsManager.Instance.FilterCards(_deck.Where(x => x.Title != _lastCard));
+                if (_waitBeforeAttack <= 0 && deck.Any(x => x.Effects.Any(e => e.Type == ActionType.DAMAGE))) // Force enemy to play a damage card at least once every 3 turns
+                {
+                    deck = deck.Where(x => x.Effects.Any(e => e.Type == ActionType.DAMAGE)).ToArray();
+                }
             }
             var card = deck[Random.Range(0, deck.Length)];
+            if (card.Effects.Any(x => x.Type == ActionType.DAMAGE || x.Type == ActionType.DAMAGE_LIMIT))
+            {
+                _waitBeforeAttack = 2;
+            }
+            else
+            {
+                _waitBeforeAttack = 0;
+            }
             _lastCard = card.Title;
             CardsManager.Instance.SpawnAICard(card);
             CardsManager.Instance.DoAction(card);
